5c6c2a50122cffdd1e9f2cd4a6f33d92ba767ace
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-06-20 05:26:28 +0300

    added a business form

--- hood/forms.py
+++ hood/forms.py
@@ -18,3 +18,8 @@ class NeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         exclude = ('admin',)
+        
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
