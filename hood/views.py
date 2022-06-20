db5fcc8a3e21c344cbe64451f6db10328f8f8e4c
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-06-20 05:41:34 +0300

    defined index in views

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from .forms import SignupForm, BusinessForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood, Profile, Business, Post
+from .forms import UpdateProfileForm, NeighbourHoodForm, PostForm
+from django.contrib.auth.models import User
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
 
 # Create your views here.
